5a46623341dbec317b46666bf3d87476f7af2a55
js l <malangtofu507@gmail.com>
2024-05-20 11:09:30 +0900

    메인 페이지 소개글, 이미지 수정 + build commit

--- src/components/Banner2.js
+++ src/components/Banner2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner2Image from '../shared/banner2.png';  // 이미지를 직접 임포트합니다.
+import banner2Image from '../shared/banner2-1.png';  // 이미지를 직접 임포트합니다.
 import '../css/Banner2.css'; // 배너2 CSS 파일 임포트
 
 const Banner2 = ({ onPopularClick, onTrustClick }) => {
@@ -8,17 +8,17 @@ const Banner2 = ({ onPopularClick, onTrustClick }) => {
             <div className="banner2-content">
                 <div className="banner2-text">
                     <h1 style={{ fontSize: '2.5rem', lineHeight: '1.35', marginBottom: '2.4rem', fontWeight: '800' }}>
-                        우리 동네
+                        당신이 더 나은 행복을 위해
                         <br />
-                        중고 직거래 마켓
+                        함께합니다.
                     </h1>
                     <p style={{ fontSize: '17px', lineHeight: '1.5' }}>
-                        동네 주민들과 가깝고 따뜻한 거래를 지금 경험해보세요.
+                        평사시 처럼 일기를 기록하듯, 버디와 함께 하루를 기록하고 성장하세요.
                     </p>
                     <div style={{ marginTop: '3.2rem' }}>
-                        <button onClick={onPopularClick} style={{ backgroundColor: '#f1f3f5', color: '#212529', border: 'none', borderRadius: '6px', padding: '15px 20px', fontSize: '18px', fontWeight: '900', margin: '0px 10px' }}>
+                        {/* <button onClick={onPopularClick} style={{ backgroundColor: '#f1f3f5', color: '#212529', border: 'none', borderRadius: '6px', padding: '15px 20px', fontSize: '18px', fontWeight: '900', margin: '0px 10px' }}>
                             인기매물 보기
-                        </button>
+                        </button> */}
                         {/* <a href="https://www.daangn.com/trust" style={{ backgroundColor: '#f1f3f5', color: '#212529', textDecoration: 'none', padding: '15px 20px', display: 'inline-block', fontSize: '18px', fontWeight: '900', margin: '0px 10px' }}>
                             믿을 수 있는 중고거래
                         </a> */}
--- src/components/Banner4.js
+++ src/components/Banner4.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner4Image from '../shared/banner4.png';  // 이미지를 직접 임포트합니다.
+import banner4Image from '../shared/Banner4.jpeg';  // 이미지를 직접 임포트합니다.
 import '../css/Banner4.css'; // 배너4 CSS 파일 임포트
 
 const Banner4 = () => {
@@ -8,14 +8,14 @@ const Banner4 = () => {
             <div className="banner4-content">
                 <div className="banner4-text">
                     <h1 style={{ fontSize: '2.5rem', lineHeight: '1.35', marginBottom: '2.4rem', fontWeight: '800' }}>
-                        내 근처에서 찾는 <br /> 우리 동네 가게
+                        당신은 더 <br /> 행복한 삶을
                     </h1>
                     <p style={{ fontSize: '17px', lineHeight: '1.5' }}>
-                        우리 동네 가게를 찾고 있나요? <br /> 동네 주민이 남긴 진짜 후기를 함께 확인해보세요!
+                        버디를 함께 한다면 <br /> 하루를 한 번 더 생각하고 감정을 추스를 수 있습니다.
                     </p>
-                    <a href="https://town.daangn.com/" style={{ backgroundColor: '#f1f3f5', color: '#212529', textDecoration: 'none', padding: '15px 20px', display: 'inline-block', fontSize: '18px', fontWeight: '900' }}>
+                    {/* <a href="https://town.daangn.com/" style={{ backgroundColor: '#f1f3f5', color: '#212529', textDecoration: 'none', padding: '15px 20px', display: 'inline-block', fontSize: '18px', fontWeight: '900' }}>
                         당근마켓 동네가게 찾기
-                    </a>
+                    </a> */}
                 </div>
                 <img src={banner4Image} alt="Banner 4" className="banner4-image" />
             </div>
